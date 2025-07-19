cdd92f17154d271131da868b7b31e77453bad668
Daniel Kantor <git@daniel-kantor.com>
2025-07-19 21:26:15 +0200

    small reorganization

---
+++ src/app/edit/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Graph from "@/components/graph";
+
+
+export default function Home() {
+  return (
+    <main className="bg-dotted w-screen h-screen">
+      <Graph />
+    </main>
+  );
+}
